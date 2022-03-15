5e3c8f77ba396b24b52b64116521e36ac4e4d34c
Andrew TOwnsley <atownsley86@gmail.com>
2022-03-14 22:11:12 -0600

    Trying to figure out why the next image will not load when the slider buttons are clicked.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import './App.scss';
 import React, { useState, useEffect } from 'react';
 import ImageModal from './components/ImageModal';
 import ModalSlide from './components/ModalSlide';
+import Loading from './components/Loading';
 
 function App() {
   const [photoData, setPhotoData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [modal, setModal] = useState(false);
   const [currentSlide, setCurrentSlide] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const fetchPhotoData = async () => {
+      setIsLoading(true);
       const data = await fetch(`https://api.pexels.com/v1/curated?per_page=10`, {
         headers: {
           Authorization: "563492ad6f91700001000001f58e6def40a2436c823881c0b23a45b7"
@@ -17,15 +21,16 @@ function App() {
       })
       const response = await data.json();
       setPhotoData(response.photos);
-      console.log(response.photos);
+      console.log("photoData: ", photoData);
+      setIsLoading(false);
     }
     fetchPhotoData();
-  }, [])
+  }, [setModal])
 
   const openModal = (image,index) => {
-    setCurrentSlide(image.src.medium)
+    setCurrentSlide(image)
+    setCurrentIndex(index)
     setModal(true);
-    console.log(currentSlide);
   }
 
   return (
@@ -35,8 +40,9 @@ function App() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ipsam fuga dignissimos. Voluptatem, voluptates. Voluptas, ipsum? Laboriosam dolore quod minus.</p>
         <section className='photos'>
           {
+            isLoading ? <Loading /> :
             photoData.map((image, index) => (
-              <article onClick={(index) => openModal(image, index)} className='photo-card' key={index}>
+              <article onClick={() => openModal(image, index)} className='photo-card' key={index}>
                 <img className='photo-img' alt={image.alt} src={image.src.medium}/>
                 <div>
                   <p>Lorem, ipsum.</p>
@@ -52,6 +58,10 @@ function App() {
         photoData={photoData}
         setModal={setModal}
         currentSlide={currentSlide} 
+        currentIndex={currentIndex}
+        setCurrentIndex={setCurrentIndex}
+        isLoading={isLoading}
+        setIsLoading={setIsLoading}
       />
       }
     </div>
--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -1,11 +1,71 @@
 import React from 'react'
-import ModalSlide from './ModalSlide'
+import { MdArrowBackIos, MdArrowForwardIos } from 'react-icons/md'
+import Loading from './Loading'
 
-const ImageModal = ({ photoData, setModal, currentSlide }) => {
+
+const ImageModal = ({ photoData, setModal, currentSlide, setCurrentSlide, currentIndex, setCurrentIndex, isLoading, setIsLoading }) => {
+
+    
+  // Tge problem is that the newItem is not loaded yet, so the image is not loaded.
+  // The solution is to use the setTimeout function to wait for the newItem to be loaded.
+  // or use the useEffect function to wait for the newItem to be loaded.
+  
+  const prevModalSlide = () => {
+    //   setIsLoading(true)
+      const totalLength = photoData.length;
+      if (currentIndex === 0) {
+      setCurrentIndex(totalLength + 1);
+      const newUrl = photoData[totalLength + 1];
+      setCurrentSlide(newUrl);
+        setIsLoading(false);
+      return;
+    } else {
+        const newIndex = currentIndex + 1;
+        const newUrl = photoData.filter((item) => {
+            return photoData.indexOf(item) === newIndex;
+        });
+        const newItem = newUrl[0];
+        console.log("newItem: ",newItem);
+        setCurrentIndex(newIndex);
+        // setCurrentSlide(newItem)
+        // setIsLoading(false);
+        console.log("currentSlide: ", currentSlide);
+    }
+    }
+    
+    const nextModalSlide = () => {
+        const totalLength = photoData.length;
+        if (currentIndex === 0) {
+          setCurrentIndex(totalLength - 1);
+          const newUrl = photoData[totalLength - 1];
+          setCurrentSlide(newUrl);
+          console.log("newUrl: ",newUrl);
+          return;
+        } else {
+            const newIndex = currentIndex - 1;
+            const newUrl = photoData.filter((item) => {
+                return photoData.indexOf(item) === newIndex;
+            });
+            const newItem = newUrl[0];
+            console.log("newItem: ",newItem);
+            setCurrentIndex(newIndex);
+            // setCurrentSlide(newItem)
+            console.log("currentSlide: ",currentSlide);
+        }
+    }
+    
+    console.log("currentSlide:", currentSlide);
+    console.log("currentIndex:", currentIndex);
 
   return (
-    <div className='image-modal' onClick={() => setModal(false)}>
-        <img src={currentSlide} alt="" />
+    <div className='image-modal'>
+        <button onClick={() => setModal(false)}>Close</button>
+        <button onClick={prevModalSlide}><MdArrowBackIos/></button>
+        {
+            isLoading ? <Loading /> : 
+            <img src={currentSlide.src.medium} alt="" />
+        }
+        <button onClick={nextModalSlide}><MdArrowForwardIos/></button>
     </div>
   )
 }
