6fd963531ad6eeb3b65f625878b79876e330ab50
Andrew TOwnsley <atownsley86@gmail.com>
2022-03-22 16:56:53 -0600

    Fixed bug from returning no results.

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function App() {
             <h1>SHUTTERSEARCH</h1>
           </div>
         </header>
-          <h3>{photoData.length ? search : 'No Photos found for that search term.'}</h3>
+          <h3 className='search-term'>{(photoData.length || isLoading) ? search : 'No Photos found for that search term.'}</h3>
         <section className='photos'>
           {
             (isLoading && !modal) ? <Loading /> :
